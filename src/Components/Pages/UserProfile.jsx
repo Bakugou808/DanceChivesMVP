9aa5a71e8ef5d9eb8dfeaafd4d3282d157276405
brent <brent4994@hotmail.com>
2021-07-14 21:12:19 -0700

    Updates to user profile page

--- src/Components/Pages/UserProfile.jsx
+++ src/Components/Pages/UserProfile.jsx
@@ -1,29 +1,45 @@
-import React from "react";
+import React from 'react';
 
 const UserProfile = () => {
-  return <div className="card">
- <div className="userProfile-cont container">
-  <div className="card-content">
-    <div className="media">
-      <div className="media-left">
-        <figure className="image is-48x48">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-        </figure>
-      </div>
-      <div className="media-content">
-        <p className="title is-4">John Smith</p>
-        <p className="subtitle is-6">@johnsmith</p>
-      </div>
-    </div>
+    return (
+        <div class="container">
+            <div class="card">
+                <div class="card-content">
+                    <div class="media">
+                        <div class="media-left">
+                            <figure class="image is-48x48">
+                                <img
+                                    src="https://bulma.io/images/placeholders/96x96.png"
+                                    alt="Placeholder image"
+                                />
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4">John Smith</p>
+                            <p class="subtitle is-6">@johnsmith</p>
+                        </div>
+                    </div>
 
-    <div className="content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus nec iaculis mauris.
-      <br />
-    </div>
-  </div>
-  </div>
-</div>;
+                    <div class="content">
+                        Add a description with your own
+                        <a href="#"> #ownedtag</a>
+                        <br />
+                        <ul>
+                            <a href="#">
+                                <li>Wins</li>
+                            </a>
+                            <a href="#">
+                                <li>Attended Events</li>
+                            </a>
+                            <a href="#">
+                                <li>Battles</li>{' '}
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default UserProfile;
